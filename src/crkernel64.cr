cc5ca9e4817f8af1023da867ff30c8761c6c5564
Arnaud Fernandés <arnaud.fernandes@laposte.net>
2016-11-21 13:46:53 +0100

    rename crkernel64.cr to main.cr

--- src/crkernel64.cr
+++
@@ -1,5 +0,0 @@
-require "./crkernel64/*"
-
-module Crkernel64
-  # TODO Put your code here
-end
---
+++ src/main.cr
@@ -0,0 +1,7 @@
+require "./crkernel64/*"
+
+def kernel_main
+  a = "Kernel String"
+end
+
+kernel_main
